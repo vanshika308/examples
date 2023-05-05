5f1d35bc2491fad6d43e22adb45220ecdbd197c3
vanshika308 <vanshikatiwari0308@gmail.com>
2023-05-05 15:45:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//Examine the document object
+//console.dir(document);
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.borderBottom='solid 3px black';
+var title=document.getElementById('title');
+title.style.color='green';
