944d2833f93991cfdf12d3fa90c94021e099f07b
vanshika308 <vanshikatiwari0308@gmail.com>
2023-05-05 17:30:58 +0530

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Examine the document object
 //console.dir(document);
-var headerTitle = document.getElementById('header-title');
-headerTitle.style.borderBottom='solid 3px black';
-var title=document.getElementById('title');
-title.style.color='green';
+var items = document.getElementsByClassName('list-group-items');
+console.log(items);
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
